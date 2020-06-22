4659ef33b3e21d340206eaa0d6dda7f0c2ab54be
Lucas <54042430+LucasGitHubz@users.noreply.github.com>
2020-06-22 11:16:33 +0200

    Initial Commit

---
+++ Reciplease/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reciplease
+//
+//  Created by kuroro on 22/06/2020.
+//  Copyright © 2020 lucasam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
