03af0416612085a3778ad794089ac77bd8f15d16
Lucas <54042430+LucasGitHubz@users.noreply.github.com>
2020-07-05 16:12:18 +0200

    RecipeData Tests

--- RecipleaseTests/RecipeDataTests.swift
+++ RecipleaseTests/RecipeDataTests.swift
@@ -8,12 +8,37 @@
 
 import XCTest
 @testable import Reciplease
+import CoreData
 
 class RecipeDataTests: XCTestCase {
-    var recipeService: RecipeService!
+    let recipeName = "Brioche"
+    let ingredient = ["farine", "oeufs", "sucre"]
+    let time = "10"
+    let yield = "4"
+    let image = "https://www.edamam.com/web-img/a10/a10c3d635c6b045750217609a28a474a.jpg"
 
-    override func setUp() {
-        super.setUp()
-        recipeService = RecipeService()
+    func testWhenAddingNewValueToRecipeDataEntityThenRecipeDataCountUp() {
+        let testRecipeDataCount = RecipeData.allRecipesData.count
+
+        RecipeData.saveRecipeData(recipeName, ingredient, time, yield, image)
+        print("recipedata count : \(RecipeData.allRecipesData.count)")
+
+        XCTAssertTrue(RecipeData.allRecipesData.count > testRecipeDataCount)
+    }
+
+    func deleteAllDataFromRecipeDataEntity() {
+        RecipeData.allRecipesData.forEach { AppDelegate.viewContext.delete($0) }
+        
+    }
+
+    func testWhenDeletingValueFromRecipeDataEntityThenThisValueWas() {
+        deleteAllDataFromRecipeDataEntity()
+        print("recipedata count : \(RecipeData.allRecipesData.count)")
+
+        RecipeData.saveRecipeData(recipeName, ingredient, time, yield, image)
+
+        RecipeData.deleteRecipeData("Brioche")
+
+        XCTAssertTrue(RecipeData.allRecipesData.count == 0)
     }
 }
