5671d9e648b5b49bde2cf30b25403bdfabf69d21
Lucas <54042430+LucasGitHubz@users.noreply.github.com>
2020-07-06 10:25:08 +0200

    makeIngredientTab function in RecipeService factored
    - Usage of compactMap

--- Reciplease/Service/RecipeService.swift
+++ Reciplease/Service/RecipeService.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class RecipeService {
     // MARK: Methods
-
+    
     // This method returns, if successful, the recipesData to be used in RecipListVC. Else, displays the corresponding error
     func getRecipeData(userIngredients: String, callBack: @escaping (Bool, FinalRecipe?) -> Void) {
         WrapperAPI.shared.perform(userIngredient: userIngredients, decode: ResponseRecipe.self) { (result) in
@@ -23,36 +23,32 @@ class RecipeService {
                     callBack(false, nil)
                     return
                 }
-
+                
                 let names = recipesData.hits.compactMap { $0.recipe?.label }
                 let ingredients = self.makeIngredientTab(with: recipesData)
                 let yield = recipesData.hits.compactMap { $0.recipe?.yield }
                 let time = recipesData.hits.compactMap { $0.recipe?.totalTime }
                 let image = recipesData.hits.compactMap { $0.recipe?.image }
-
+                
                 let completRecipe = FinalRecipe(name: names, ingredient: ingredients, time: time, yield: yield, image: image)
+                
                 callBack(true, completRecipe)
             }
         }
     }
-
-    // This method transform an array [[Ingredients]?] to an array [[String]], its takes juste the name of each ingredients and creates array with it.
+    
+    // This method transform an array [[Ingredients]?] to an array [[String]], it takes juste the name of each ingredients and creates array with it.
     private func makeIngredientTab(with: ResponseRecipe) -> [[String]] {
         var ingredients = [[Ingredients]?]()
-        var ingredientTab = [String]()
         var finalIngredientTab = [[String]]()
         
-        // Creating an array of Ingredient arrays
+        // Creating an array of [Ingredient arrays]
         ingredients = with.hits.compactMap { $0.recipe?.ingredients }
-
-        // Creating a String array of each Ingredients array contained in the array of Ingredients array (ingredients)
+        
+        // Creating a String array of each [Ingredients array] contained in ingredients (of type: [[Ingredients]])
         // Then adding this String array to an array of String array
-        for index in ingredients {
-            ingredientTab.removeAll()
-            for i in 0...(index?.count ?? 0) - 1 {
-                ingredientTab.append(index?[i].text ?? "")
-            }
-            finalIngredientTab.append(ingredientTab)
+        for ingredientTab in ingredients {
+            finalIngredientTab.append(ingredientTab?.compactMap { $0.text } ?? [])
         }
         return finalIngredientTab
     }
